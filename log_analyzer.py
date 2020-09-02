b6d33dacf690be0260562fbaddbcb9ae375d302a
Dmitry Budyakov <dmitry130590@gmail.com>
2020-09-02 21:49:53 +0300

    dev

--- log_analyzer.py
+++ log_analyzer.py
@@ -10,28 +10,49 @@
 from collections import namedtuple
 import datetime
 import gzip
+import logging
 import os
+import re
 from string import Template
 
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./log",
+    "VALID_LOG_FORMATS": ["gz", "log"],
 }
 
 
-def get_date_from_logname(logname):
-    """Парсит имя лога и вытаскивает дату"""
-    name = logname.rstrip(".gz")
-    date_str = name.split("-")[-1]
-    year = date_str[:4]
-    month = date_str[4:6]
-    day = date_str[6:]
+def is_ui_log(file_name):
+    """
+    Проверяет, что данный лог является логом интерфейса
+    """
+    name = file_name.split(".")[0]
+    type_ = name.split("-")[-1]
+    return type_ == "ui"
+
+
+def is_valid_format(file_name, valid_formats):
+    """Проверяет, что лог имеет корректный формат"""
+    suf = file_name.split(".")[-1]
+    for ext in valid_formats:
+        if suf.startswith(ext):
+            return True
+
+    return False
+
+
+def get_date_from_log_name(log_name):
+    """Вытаскивает дату создания из имени лога"""
+    date = re.search(r"\d+", log_name).group()
+    year = date[:4]
+    month = date[4:6]
+    day = date[6:]
     return datetime.date(year=int(year), month=int(month), day=int(day))
 
 
-def get_latest_log_file(log_dir):
-    """Достает последний файл лога"""
+def get_latest_log_file(log_dir, valid_formats):
+    """Просматривает папку с логами и находит самый свежий"""
     Result = namedtuple("Result", ["name", "path", "date"])
     result = Result(name=None, path=None, date=None)
     empty_result = result
@@ -43,11 +64,14 @@ def get_latest_log_file(log_dir):
         if not os.path.isfile(path):
             continue
 
-        if result.name is None:
-            result = Result(name=name, path=path, date=get_date_from_logname(name))
+        if not is_ui_log(name) or not is_valid_format(name, valid_formats):
             continue
 
-        current_date = get_date_from_logname(name)
+        if result.name is None:
+            result = Result(name=name, path=path, date=get_date_from_log_name(name))
+            continue
+
+        current_date = get_date_from_log_name(name)
         if current_date > result.date:
             result = Result(name=name, path=path, date=current_date)
 
@@ -57,18 +81,54 @@ def get_latest_log_file(log_dir):
     return result
 
 
-def get_params(logfile):
-    with gzip.open(logfile.path, 'r') as file:
+def get_request_params(logfile):
+    opener = gzip if logfile.name.endswitg("gz") else open
+    with opener.open(logfile.path, 'r') as file:
         for row in file:
-            yield row
+            request = row.decode(encoding="utf-8")
+            request_url = re.search(r"\"(GET|POST|PUT|HEAD|OPTIONS)\s\S+", request)
+            if request_url is None:
+                logging.warning(row)
+                continue
+            request_time = re.search(r"\s\d+\.\d+\s", request)
+            if request_time is None:
+                logging.warning(row)
+            time = float(request_time.group())
+            url = request_url.group().split()[-1]
+            yield url, time
 
 
-def render_template(logfile):
-    # row = [{"count": 2767, "time_avg": 62.994999999999997, "time_max": 9843.5689999999995, "time_sum": 174306.35200000001, "url": "/api/v2/internal/html5/phantomjs/queue/?wait=1m", "time_med": 60.073, "time_perc": 9.0429999999999993, "count_perc": 0.106}]
+def get_table_json(logfile):
+    data = {}
+    count_total_req = 0
+    request_time_sum = 0
+    for url, time in get_request_params(logfile):
+        count_total_req += 1
+        request_time_sum += time
+        if url not in data:
+            data[url] = {"count": 1, "time_sum": time, "time_max": time, "url": url}
+        else:
+            data_url = data[url]
+            data_url["count"] += 1
+            data_url["time_sum"] += time
+            if time > data_url["time_max"]:
+                data_url["time_max"] = time
+
+    result = []
+    for val in data.values():
+        val["count_perc"] = (val["count"] / count_total_req) * 100
+        val["time_perc"] = (val["time_sum"] / request_time_sum) * 100
+        val["time_avg"] = val["time_sum"] / val["count"]
+        result.append(val)
+
+    return result
+
+
+def render_template(table_json):
     f = open("report.html", "r")
     tmp = f.read()
     t = Template(tmp)
-    report = t.safe_substitute(table_json=[row for row in get_params(logfile)])
+    report = t.safe_substitute(table_json=table_json)
     f.close()
     f = open("test.html", "w")
     f.write(report)
@@ -76,9 +136,9 @@ def render_template(logfile):
 
 
 def main():
-    logfile = get_latest_log_file(config["LOG_DIR"])
-    # get_params(logfile)
-    render_template(logfile)
+    logfile = get_latest_log_file(config["LOG_DIR"], config["VALID_LOG_FORMATS"])
+    table_json = get_table_json(logfile)
+    render_template(table_json)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -23,12 +23,55 @@ class TestDefaultConfig(unittest.TestCase):
         self.assertIn("LOG_DIR", config, msg="'LOG_DIR' not in default config")
 
 
-class TestDateFromLogname(unittest.TestCase):
-    logname_gz = "nginx-access-ui.log-20170630.gz"
+class TestValidLogFormat(unittest.TestCase):
+    valid_log_formats = ["gz", "log"]
+    name_gz = "some_log.gz"
+    name_log = "some_log.log-20190630"
+    name = "some_log"
+    name_bz2 = "some_log.bz2"
 
-    def test_logname_gz(self):
-        real_date = log_analyzer.get_date_from_logname(self.logname_gz)
+    def test_gz(self):
+        res = log_analyzer.is_valid_format(self.name_gz, self.valid_log_formats)
+        self.assertTrue(res)
+
+    def test_log(self):
+        res = log_analyzer.is_valid_format(self.name_log, self.valid_log_formats)
+        self.assertTrue(res)
+
+    def test_empty(self):
+        res = log_analyzer.is_valid_format(self.name, self.valid_log_formats)
+        self.assertFalse(res)
+
+    def test_bz2(self):
+        res = log_analyzer.is_valid_format(self.name_bz2, self.valid_log_formats)
+        self.assertFalse(res)
+
+
+class TestIsUiLog(unittest.TestCase):
+    ui_log = "nginx-access-ui.log-20190312"
+    not_ui_log = "nginx-access-blabla.log-20180301"
+
+    def test_affirmative(self):
+        res = log_analyzer.is_ui_log(self.ui_log)
+        self.assertTrue(res)
+
+    def test_negative(self):
+        res = log_analyzer.is_ui_log(self.not_ui_log)
+        self.assertFalse(res)
+
+
+class TestDateFromLogname(unittest.TestCase):
+    log_name_gz = "nginx-access-ui.log-20170630.gz"
+    log_name_plain = "nginx-access-ui.log-20180421"
+
+    def test_gz(self):
+        real_date = log_analyzer.get_date_from_log_name(self.log_name_gz)
         expected_date = datetime.date(year=2017, month=6, day=30)
+        self.assertEqual(real_date, expected_date)
+
+    def test_plain(self):
+        real_date = log_analyzer.get_date_from_log_name(self.log_name_plain)
+        expected_date = datetime.date(year=2018, month=4, day=21)
         self.assertEqual(real_date, expected_date)
 
 
@@ -36,6 +79,9 @@ class TestLatestLogFile(unittest.TestCase):
     not_existing_dir = "/tmp/log_analyzer/not_existing"
     empty_log_dir = "/tmp/log_analyzer/empty"
     log_dir = "/tmp/log_analyzer/log_dir"
+
+    valid_formats = ["gz", ".og"]
+    log_map = {}
 
     @staticmethod
     def _get_random_datetime():
@@ -45,23 +91,22 @@ class TestLatestLogFile(unittest.TestCase):
         delta_days = delta.days
         return start_date + datetime.timedelta(days=random.randrange(delta_days))
 
-    @staticmethod
-    def _get_log_date(filename):
-        fn = filename.rstrip(".gz")
-        date_str = fn.split("-")[-1]
-        year, month, day = int(date_str[:4]), int(date_str[4:6]), int(date_str[6:])
-        return datetime.date(year=year, month=month, day=day)
+    # @staticmethod
+    # def _get_log_date(filename):
+    #     fn = filename.rstrip(".gz")
+    #     date_str = fn.split("-")[-1]
+    #     year, month, day = int(date_str[:4]), int(date_str[4:6]), int(date_str[6:])
+    #     return datetime.date(year=year, month=month, day=day)
 
-    def _create_ui_logs(self, count=10, log_format=None):
-        if log_format is None:
-            log_format = ".gz"
+    def _create_logs(self, count=10, log_type="ui", log_format=None):
         for _ in range(count):
             date = self._get_random_datetime()
             month = f"{date.month}" if date.month >= 10 else f"0{date.month}"
             day = f"{date.day}" if date.day >= 10 else f"0{date.day}"
-            name = f"nginx-access-ui.log-{date.year}{month}{day}{log_format}"
+            name = f"nginx-access-{log_type}.log-{date.year}{month}{day}{log_format}"
+            self.log_map[date] = name
             path = os.path.join(self.log_dir, name)
-            if log_format == ".gz":
+            if log_format == "gz":
                 with gzip.open(path, "wb") as f:
                     f.write(b"some content")
             else:
@@ -75,39 +120,41 @@ class TestLatestLogFile(unittest.TestCase):
 
         os.makedirs(self.empty_log_dir)
         os.makedirs(self.log_dir)
-        self._create_ui_logs()
+        self.log_map = {}
+        self._create_logs()
 
     def test_log_dir_is_not_exist(self):
-        log = log_analyzer.get_latest_log_file(self.not_existing_dir)
+        log = log_analyzer.get_latest_log_file(self.not_existing_dir, self.valid_formats)
         self.assertIsInstance(log, tuple)
         self.assertIsNone(log.name)
         self.assertIsNone(log.path)
         self.assertIsNone(log.date)
 
     def test_log_dir_is_empty(self):
-        log = log_analyzer.get_latest_log_file(self.empty_log_dir)
+        log = log_analyzer.get_latest_log_file(self.empty_log_dir, self.valid_formats)
         self.assertIsInstance(log, tuple)
         self.assertIsNone(log.name)
         self.assertIsNone(log.path)
         self.assertIsNone(log.date)
 
     def test_return_format(self):
-        log = log_analyzer.get_latest_log_file(self.log_dir)
+        log = log_analyzer.get_latest_log_file(self.log_dir, self.valid_formats)
         self.assertIsInstance(log, tuple, msg="Incorrect return type")
 
     def test_latest_log(self):
         files = []
         for _, _, filenames in os.walk(self.log_dir):
             files.extend(filenames)
-        sorted_files = sorted(files, key=self._get_log_date, reverse=True)
-        latest_log = sorted_files[0]
+        sorted_dates = sorted(self.log_map.keys())
+        latest_date = sorted_dates[0]
+        latest_log = self.log_map[latest_date]
         Latest = namedtuple("Latest", ["name", "path", "date"])
         expected_latest = Latest(
             name=latest_log,
             path=os.path.join(self.log_dir, latest_log),
-            date=self._get_log_date(latest_log)
+            date=latest_date,
         )
-        real_latest = log_analyzer.get_latest_log_file(self.log_dir)
+        real_latest = log_analyzer.get_latest_log_file(self.log_dir, self)
         self.assertEqual(expected_latest, real_latest, msg="Function return not latest log file")
 
     # 3 папка есть, содержимое есть, но нужных логов нет
@@ -116,7 +163,8 @@ class TestLatestLogFile(unittest.TestCase):
 
 class TestMain(unittest.TestCase):
     def test_main(self):
-        log_analyzer.main()
+        pass
+        # log_analyzer.main()
 
 
 if __name__ == "__main__":
